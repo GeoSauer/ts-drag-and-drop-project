702da99ce3cf48e1fa068728c9e7953cd83838f3
GeoSauer <geoffrey.sauer89@gmail.com>
2023-08-01 13:48:08 -0600

    adds inheritance and generics

--- src/app.ts
+++ src/app.ts
@@ -15,14 +15,30 @@ class Project {
 }
 
 //? Project State Management
-type Listener = (items: Project[]) => void;
+//* the below refactor is more for larger applications that could have multiple states and thus more redundant code
+// type Listener = (items: Project[]) => void;
+type Listener<T> = (items: T[]) => void;
 
-class ProjectState {
-  private listeners: Listener[] = [];
+class State<T> {
+  // private listeners: Listener<T>[] = [];
+  //* changing from private to protected to allow child classes access
+  protected listeners: Listener<T>[] = [];
+
+  addListener(listenerFunction: Listener<T>) {
+    this.listeners.push(listenerFunction);
+  }
+}
+
+class ProjectState extends State<Project> {
+  //? moved into the base State class
+  // private listeners: Listener[] = [];
   private projects: Project[] = [];
   private static instance: ProjectState;
 
-  private constructor() {}
+  private constructor() {
+    //* as always, call super to inherit the parent constructor
+    super();
+  }
 
   static getInstance() {
     if (this.instance) {
@@ -32,10 +48,10 @@ class ProjectState {
     return this.instance;
     // return (this.instance ||= new ProjectState());
   }
-
-  addListener(listenerFunction: Listener) {
-    this.listeners.push(listenerFunction);
-  }
+  //? moved into the base State class
+  // addListener(listenerFunction: Listener) {
+  //   this.listeners.push(listenerFunction);
+  // }
 
   addProject(title: string, description: string, numOfPeople: number) {
     const newProject = new Project(
@@ -107,24 +123,76 @@ function validate(validatableInput: Validatable) {
   return isValid;
 }
 
-//? ProjectList Class
-class ProjectList {
+//? Component Base Class
+//* we make this an abstract class because we don't want anyone to be able to directly instantiate it
+abstract class Component<T extends HTMLElement, U extends HTMLElement> {
   templateElement: HTMLTemplateElement; //* everything between the <template> tags
-  hostElement: HTMLDivElement; //* the <div> where we want to render the templateElement
-  sectionElement: HTMLElement; //* there is no HTMLSectionElement so we go generic here
+  hostElement: T; //* we don't know what concrete type this and the below element will be,
+  element: U; //* so we define some generic types in the class definition to handle it
+
+  constructor(
+    templateId: string,
+    hostElementId: string,
+    insertAtStart: boolean,
+    newElementId?: string //* optional params should always be the last params
+  ) {
+    this.templateElement = document.getElementById(templateId)! as HTMLTemplateElement;
+    this.hostElement = document.getElementById(hostElementId)! as T; //* typecasting since we don't necessarily know what the element will be
+
+    const importedNode = document.importNode(this.templateElement.content, true);
+    this.element = importedNode.firstElementChild as U; //* typecasting since we don't necessarily know what the element will be
+    if (newElementId) {
+      this.element.id = newElementId;
+    }
+
+    this.attachToDom(insertAtStart);
+  }
+
+  private attachToDom(insertAtBeginning: boolean) {
+    this.hostElement.insertAdjacentElement(
+      insertAtBeginning ? "afterbegin" : "beforeend",
+      this.element
+    );
+  }
+
+  //* adding these shows anyone else who works on the codebase that while the attaching happens here, the concrete configuration and rendering happens where this class is inherited
+  //* sidenote, private abstract classes are not a thing allowed by TS
+  abstract configure(): void;
+  abstract renderContent(): void;
+}
+
+//? ProjectList Class
+//* we add 'extends Component' to cut down on duplicate code and increase reusability
+class ProjectList extends Component<HTMLDivElement, HTMLElement> {
+  //?this stuff is now in the Component class
+  // templateElement: HTMLTemplateElement; //* everything between the <template> tags
+  // hostElement: HTMLDivElement; //* the <div> where we want to render the templateElement
+  // sectionElement: HTMLElement; //* there is no HTMLSectionElement so we go generic here
   assignedProjects: Project[]; //* making this an array of our Project type adds great autocomplete and typo checks
   // assignedProjects: Any[];
 
   //* using the 'private' shortcut to add the project type
   constructor(private type: "active" | "finished") {
-    this.templateElement = document.getElementById("project-list")! as HTMLTemplateElement;
-    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+    //* now we call super at the beginning to call the constructor of the base class, Component
+    super("project-list", "app", false, `${type}-projects`);
+    //?this stuff is now in the Component class
+    // this.templateElement = document.getElementById("project-list")! as HTMLTemplateElement;
+    // this.hostElement = document.getElementById("app")! as HTMLDivElement;
     this.assignedProjects = [];
 
-    const importedNode = document.importNode(this.templateElement.content, true);
-    this.sectionElement = importedNode.firstElementChild as HTMLElement;
-    this.sectionElement.id = `${this.type}-projects`; //* dynamically sets the id
+    //?this stuff is now in the Component class
+    // const importedNode = document.importNode(this.templateElement.content, true);
+    // this.sectionElement = importedNode.firstElementChild as HTMLElement;
+    // this.sectionElement.id = `${this.type}-projects`; //* dynamically sets the id
 
+    //? also happens in Component class now
+    // this.attachToDom();
+    this.configure();
+    this.renderContent();
+  }
+
+  //* not a must do, but convention is to have all public methods right below the constructor and before any private methods
+  configure() {
     projectState.addListener((projects: Project[]) => {
       //* to fix the entire project list being rendered in both active and finished, we use the filter method
       const relevantProjects = projects.filter((prj) => {
@@ -140,9 +208,14 @@ class ProjectList {
       this.assignedProjects = relevantProjects;
       this.renderProjects();
     });
+  }
 
-    this.attachToDom();
-    this.renderContent();
+  //* we have to remove 'private' here so that the base class can access it
+  // private renderContent() {
+  renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector("ul")!.id = listId;
+    this.element.querySelector("h2")!.textContent = this.type.toUpperCase() + " PROJECTS";
   }
 
   private renderProjects() {
@@ -157,52 +230,61 @@ class ProjectList {
     }
   }
 
-  private renderContent() {
-    const listId = `${this.type}-projects-list`;
-    this.sectionElement.querySelector("ul")!.id = listId;
-    this.sectionElement.querySelector("h2")!.textContent = this.type.toUpperCase() + " PROJECTS";
-  }
-
-  private attachToDom() {
-    //* this accesses the app <div> and inserts the section before the closing tag, so after the input form
-    this.hostElement.insertAdjacentElement("beforeend", this.sectionElement);
-  }
+  //? also happens in Component class now
+  // private attachToDom() {
+  //   //* this accesses the app <div> and inserts the section before the closing tag, so after the input form
+  //   this.hostElement.insertAdjacentElement("beforeend", this.sectionElement);
+  // }
 }
 
 //? ProjectInput Class
-class ProjectInput {
-  templateElement: HTMLTemplateElement; //* everything between the <template> tags
-  hostElement: HTMLDivElement; //* the <div> where we want to render the templateElement
-  formElement: HTMLFormElement; //* this is the fist child of the <template>
+class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
+  //?this stuff is now in the Component class
+  // templateElement: HTMLTemplateElement; //* everything between the <template> tags
+  // hostElement: HTMLDivElement; //* the <div> where we want to render the templateElement
+  // formElement: HTMLFormElement; //* this is the fist child of the <template>
   titleInputElement: HTMLInputElement;
   descriptionInputElement: HTMLInputElement; //* the three inputs
   peopleInputElement: HTMLInputElement;
 
   constructor() {
+    //* once again calling super to call the constructor of the base class
+    super("project-input", "app", true, "user-input");
     //* using <> typecasting tells TS that what comes after is of that specific type
     //? this.templateElement = <HTMLTemplateElement>document.getElementById('project-input')!;
     //* the other method is to add 'as WhateverTheElementTypeIs' at the end
     //* the bang tells TS emphatically that the element will not be null
-    this.templateElement = document.getElementById("project-input")! as HTMLTemplateElement;
-    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+    //?this stuff is now in the Component class
+    // this.templateElement = document.getElementById("project-input")! as HTMLTemplateElement;
+    // this.hostElement = document.getElementById("app")! as HTMLDivElement;
 
     //* importNode() is global method available on document
     //* what this line is doing is importing everything between the <template> tags and storing it in importedNode. I'd probably call it something else, but this helps illustrate where it's coming from
     //* importNode takes a second argument, a boolean defining if it should do a deep clone, ie all levels of nesting
-    const importedNode = document.importNode(this.templateElement.content, true);
-    this.formElement = importedNode.firstElementChild as HTMLFormElement; //* hover for deets
-    this.formElement.id = "user-input"; //* assigning an id to the form element here rather than in index.html, for whatever reason. Just shows you could also do it here I guess?
+    //?this stuff is now in the Component class
+    // const importedNode = document.importNode(this.templateElement.content, true);
+    // this.formElement = importedNode.firstElementChild as HTMLFormElement; //* hover for deets
+    // this.formElement.id = "user-input"; //* assigning an id to the form element here rather than in index.html, for whatever reason. Just shows you could also do it here I guess?
 
     //* gotta use querySelector on these folks even though they are being grabbed by id
-    this.titleInputElement = this.formElement.querySelector("#title") as HTMLInputElement;
-    this.descriptionInputElement = this.formElement.querySelector(
-      "#description"
-    ) as HTMLInputElement;
-    this.peopleInputElement = this.formElement.querySelector("#people") as HTMLInputElement;
+    this.titleInputElement = this.element.querySelector("#title") as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector("#description") as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector("#people") as HTMLInputElement;
 
     this.configure(); //* here we call the private methods within the constructor
-    this.attachToDom();
+    // this.attachToDom();
   }
+
+  //* this must also be changed to public to satisfy TS
+  //* not a must do, but convention is to have all public methods right below the constructor and before any private methods
+  configure() {
+    //* depending on the project, just binding this can work fine and be less work, we're just demonstrating how you can add a method decorator to streamline things if it needed to happen in many places
+    // this.formElement.addEventListener("submit", this.submitHandler.bind(this));
+    this.element.addEventListener("submit", this.submitHandler);
+  }
+
+  //* this just needs to be added due to it being abstract in the base class and to make TS happy
+  renderContent() {}
 
   //* the thought process behind these private methods is using the constructor for the rough gathering of all necessary pieces, and then private methods for fine tuning an output
   //* again, private methods can only be called from within the class they are defined in
@@ -271,16 +353,11 @@ class ProjectInput {
     }
   }
 
-  private configure() {
-    //* depending on the project, just binding this can work fine and be less work, we're just demonstrating how you can add a method decorator to streamline things if it needed to happen in many places
-    // this.formElement.addEventListener("submit", this.submitHandler.bind(this));
-    this.formElement.addEventListener("submit", this.submitHandler);
-  }
-
-  private attachToDom() {
-    //* this accesses the app <div> and inserts the formElement after the opening tag
-    this.hostElement.insertAdjacentElement("afterbegin", this.formElement);
-  }
+  //? now handled in the base class
+  // private attachToDom() {
+  //   //* this accesses the app <div> and inserts the formElement after the opening tag
+  //   this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  // }
 }
 
 const projInput = new ProjectInput();
