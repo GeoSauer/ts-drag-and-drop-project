4caaa46f131bd9baa710e519758eaa5e5d073b3a
GeoSauer <geoffrey.sauer89@gmail.com>
2023-08-07 13:42:01 -0600

    cleans up namespace imports using them only in the necessary files

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,12 @@
 //* this is just TS magic though and doesn't work like JS imports so there will be no compilation errors but there will be runtime errors
 //* ^ this was solved by configuring the outFile in tsconfig
 //* basically our TS is split up into nice and manageable files and the compiled JS is all in one big file
-/// <reference path='models/drag-drop.ts' />
-/// <reference path='models/project.ts' />
-/// <reference path='state/project-state.ts' />
-/// <reference path='util/validation.ts' />
-/// <reference path='decorators/autobind.ts' />
+//? the following imports are really only used in other components, so it's safer to import them just where they are used instead of into this global app scope to avoid breaking things with accidental deletions etc.
+// / <reference path='models/drag-drop.ts' />
+// / <reference path='models/project.ts' />
+// / <reference path='state/project-state.ts' />
+// / <reference path='util/validation.ts' />
+// / <reference path='decorators/autobind.ts' />
 /// <reference path='components/project-input.ts' />
 /// <reference path='components/project-list.ts' />
 
